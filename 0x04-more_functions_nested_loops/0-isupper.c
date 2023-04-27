b8e0e4b5d40e63728fbbf58b5911b1a257cf1191
Carlaaramsh <carlaaramsh@gmail.com>
2023-04-27 13:11:20 -0700

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * -isupper -it will check for uppercase letters
+ *  c: the character to be checked
+ *
+ * Return 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: the first integar
+ * @b: the second integar
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
